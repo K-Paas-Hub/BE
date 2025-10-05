a5b376c55a658603726b5d16b5ef57e34c1125ce
rickyhong <ricky0130@naver.com>
2025-10-05 16:09:31 +0900

    feat: k8s 설정 수정

--- backend/src/main/java/com/app/backend/controller/TestController.java
+++ backend/src/main/java/com/app/backend/controller/TestController.java
@@ -1,20 +1,24 @@
 package com.app.backend.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 @RequestMapping("")
 public class TestController {
 
     @GetMapping("/test")
     public String showTestPage() {
+        log.info("들어옴");
         return "test";
     }
 
     @GetMapping("/kakao")
     public String showKakaoPage() {
+        log.info("카카오 들어옴");
         return "kakao";
     }
 
